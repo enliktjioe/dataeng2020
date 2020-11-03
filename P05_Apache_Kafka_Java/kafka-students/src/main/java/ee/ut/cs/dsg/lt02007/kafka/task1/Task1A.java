c2d49dd91a0871197b1661161cc7aa129c40413a
Enlik - <enlik@ut.ee>
2020-11-03 14:51:12 +0200

    Finished task 1A and 1B

--- P05_Apache_Kafka_Java/kafka-students/src/main/java/ee/ut/cs/dsg/lt02007/kafka/task1/Task1A.java
+++ P05_Apache_Kafka_Java/kafka-students/src/main/java/ee/ut/cs/dsg/lt02007/kafka/task1/Task1A.java
@@ -1,7 +1,12 @@
 package ee.ut.cs.dsg.lt02007.kafka.task1;
 
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.IntegerSerializer;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -16,12 +21,17 @@ public class Task1A {
         Properties props = new Properties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka1:9092,kafka2:9093");
         //TODO Your Producer Configuration Code Here
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
+
+
 
         String file = Task1A.class.getClassLoader().getResource(OBS).getPath();
 
         System.out.println(file);
 
         //TODO Your Producer Here
+        KafkaProducer<Integer, Integer> producer = new KafkaProducer<>(props);
 
         BufferedReader br = new BufferedReader(new FileReader(file));
 
@@ -31,6 +41,21 @@ public class Task1A {
 
             System.out.println(line);
             //TODO production code here
+            int i = 0;
+            try{
+                while (true) {
+                    i += 2;
+
+                    ProducerRecord<Integer, Integer> record = new ProducerRecord<>("obs", i, i);
+                    producer.send(record);
+                    System.out.printf("key = %d, value = %d\n", i, i);
+
+                    Thread.sleep(5000);
+                }
+
+            }finally {
+                producer.close();
+            }
         }
 
     }
